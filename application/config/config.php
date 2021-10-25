5c2a946c4a5c6164532ad14f9115e4f12dd9173d
aldoimmanuel <90598645+aldoimmanuel@users.noreply.github.com>
2021-10-26 04:12:29 +0700

    Add files via upload

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/wp2/pustaka-booking/Web';
+$config['base_url'] = 'http://localhost/wp2/pustaka-booking/';
 
 /*
 |--------------------------------------------------------------------------
